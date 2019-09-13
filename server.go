4ab9ddf10d75ebd9514355597af5b5926a7b49ab
3therk1ll <richardcurteis@users.noreply.github.com>
2019-09-13 12:06:24 +0100

    Create server.go

---
+++ server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"flag"
+)
+
+func main() {
+
+	// User arguments
+	lport := flag.Int("-port", 8000, "Local port to listen for connections")
+	cert := flag.String("-cert", false, "Certificate for TLS connection")
+	
+	if *cert == false {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+		w.Write([]byte("It works !!\n"))
+	})
+
+	b, _ := ioutil.ReadFile(cert)
+	var pemBlocks []*pem.Block
+	var v *pem.Block
+	var pkey []byte
+
+	for {
+		v, b = pem.Decode(b)
+		if v == nil {
+			break
+		}
+		if v.Type == "RSA PRIVATE KEY" {
+			if x509.IsEncryptedPEMBlock(v) {
+				pkey, _ = x509.DecryptPEMBlock(v, []byte("xxxxxxxxx"))
+				pkey = pem.EncodeToMemory(&pem.Block{
+					Type:  v.Type,
+					Bytes: pkey,
+				})
+			} else {
+				pkey = pem.EncodeToMemory(v)
+			}
+		} else {
+			pemBlocks = append(pemBlocks, v)
+		}
+	}
+	c, _ := tls.X509KeyPair(pem.EncodeToMemory(pemBlocks[0]), pkey)
+
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+		Certificates: []tls.Certificate{c},
+	}
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", lport),
+		Handler:      mux,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
+	log.Fatal(srv.ListenAndServeTLS("", ""))
+}
