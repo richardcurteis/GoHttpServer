13b0ddca5e86338fb80981b924b967f19cd3c535
Richard Curteis <richardcurteis@gmail.com>
2019-09-30 19:36:19 +0100

    fixes

--- helpers/PyCompileXPlatform.py
+++ helpers/PyCompileXPlatform.py
@@ -9,25 +9,25 @@ def get_args():
 	parser.add_argument('-o', '--os', type=str, help="Target OS")
 	parser.add_argument('-a', '--arch', type=str, help="Compilation Architecture")
 	parser.add_argument('-i', '--infile', type=str, help="infile File")
-	parser.add_argument('-o', '--output', type=str, help="Output File")
+	parser.add_argument('-w', '--write', type=str, help="Write File")
 	parser.add_argument('-p', '--print', action='store_true', help="Print OS and Architecture Compatibility")
 	return parser.parse_args()
 
 def os_and_archs():
 	return {
 		"aix": ["ppc64"],
-		"android": ["386/x86", "amd64", "arm", "arm64"],
-		"darwin":	["386/x86", "amd64", "arm", "arm64"],
+		"android": ["386", "amd64", "arm", "arm64"],
+		"darwin":	["386", "amd64", "arm", "arm64"],
 		"dragonfly": ["amd64"],
-		"freebsd": ["386/x86", "amd64", "arm"],
+		"freebsd": ["386", "amd64", "arm"],
 		"illumos": ["amd64"],
 		"js": ["wasm"],
-		"linux": ["386/x86", "amd64", "arm", "arm64", "ppc64", "ppc64le", "mips", "mipsle", "mips64", "mips64le", "s390x"],
-		"netbsd": ["386/x86", "amd64", "arm"],
-		"openbsd": ["386/x86", "amd64", "arm", "arm64"],
-		"plan9": ["386/x86", "amd64", "arm"],
+		"linux": ["386", "amd64", "arm", "arm64", "ppc64", "ppc64le", "mips", "mipsle", "mips64", "mips64le", "s390x"],
+		"netbsd": ["386", "amd64", "arm"],
+		"openbsd": ["386", "amd64", "arm", "arm64"],
+		"plan9": ["386", "amd64", "arm"],
 		"solaris": ["amd64"],
-		"windows": ["386/x86", "amd64"]
+		"windows": ["386", "amd64"]
 	}
 
 def print_archs_table():
@@ -51,14 +51,12 @@ def print_build():
 	pt.add_row(["OS", args.os])
 	pt.add_row(["Architecture", args.arch])
 	pt.add_row(["Infile", args.infile])
-	pt.add_row(["Outfile", args.outfile])
+	pt.add_row(["Outfile", args.write])
 	pt.align["Detail"] = "l"
 	print(pt)
 
 def compile(args):
-	os = args.os
-	if os is "386/x86":
-		os = '386'
+	op_sys = args.os
 	
 	# Back up current GO environment variables
 	GOARCH, GOOS = os.getenv('GOARCH'), os.getenv('GOOS')
@@ -68,12 +66,12 @@ def compile(args):
 
 	try:
 		os.environ['GOARCH'] = args.arch
-		os.environ['GOOS'] = os
+		os.environ['GOOS'] = op_sys
 
 		# Build Go binary without debug info '-s' and dwarf information, '-w'. Reduce file size.
 		# Create outfile with infile file minus '.go' extension
 		print("[*] Build binary...")
-		os.system(f'go build -ldflags="-s -w" -o {args.outfile} {args.infile}')
+		os.system(f'go build -ldflags="-s -w" -o {args.write} {args.infile}')
 
 		# Use UPX to pack binary
 		print("[*] UPX pack binary...")
@@ -102,30 +100,31 @@ if __name__ == '__main__':
 	if args.print:
 		print_archs_table()
 
-	if os.path.exists(args.infile):
-		print('[*] File exists')
-	else:
-		print("[!] Not found. Checking for 'main.go in current directory...")
-		if os.path.exists('main.go'):
-			print(["[*] main.go found in current directory. We'll build with this..."])
-			args.infile = 'main.go'
+	if args.infile is not None:
+		if os.path.exists(args.infile):
+			print('[*] File exists')
 		else:
-			exit('[*] No suitable *.go files found. Check path.')
+			print("[!] Not found. Checking for 'main.go in current directory...")
+			if os.path.exists('main.go'):
+				print(["[*] main.go found in current directory. We'll build with this..."])
+				args.infile = 'main.go'
+			else:
+				exit('[*] No suitable *.go files found. Check path.')
 
 	if args.os and args.arch and args.infile:
 		args.os = args.os.lower().strip()
 		args.arch = args.arch.lower().strip()
 		args.infile = args.infile.strip()
-		if args.output is False:
-			args.output = args.infile[0:-3]
-			print(f"[!] No outfile specified with '-o' flag. Automatically generated: {args.output}")
+		if args.write is False:
+			args.write = args.infile[0:-3]
+			print(f"[!] No outfile specified with '-o' flag. Automatically generated: {args.write}")
 		else:
-			args.output = args.output.strip()
+			args.write = args.write.strip()
 	else:
 		exit("[!] Missing arguments. Run with -h to view help.")
 	
 	if args.arch is 'x86' or '386' or '86':
-		args.arch = "386/x86"
+		args.arch = "386"
 
 	if args.os in os_and_archs().keys() and args.arch in os_and_archs()[args.os]:
 		compile(args)
